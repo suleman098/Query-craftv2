29f544f25abc93c9c78f47c260dd72fa5b21bd6c
Suleman Khan <sulemankhan@Sulemans-MacBook-Pro.local>
2025-01-06 21:03:28 +0000

    Added shadow, outline, and border to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center h-20 bg-slate-300 text-black">
+    <footer className="flex justify-center items-center h-20 bg-white text-black  shadow-xl border-grey border-2">
+
       <div>
         <p>
           Made by
           <a
             href="https://your-website-link.com"
-            className="text-blue-400 hover:text-blue-600 ml-1 "
+            className="text-blue-400 hover:text-blue-600 ml-1"
           >
             Suleman.K
           </a>
